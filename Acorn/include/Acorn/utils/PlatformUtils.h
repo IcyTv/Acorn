3978aa15b3fd2ba07e16d785d45b99561130067c
Michael Finger <michael.finger@icytv.de>
2022-02-23 20:16:05 +0100

    Acorn+OakTree: Changed FileUtils to be cross-platform
    Using portable-file-dialogs, we changed the Acorn file dialogs to be
    cross-platform.

--- Acorn/include/Acorn/utils/PlatformUtils.h
+++ Acorn/include/Acorn/utils/PlatformUtils.h
@@ -10,7 +10,7 @@ namespace Acorn
 	class PlatformUtils
 	{
 	public:
-		static std::string OpenFile(const char* filter);
-		static std::string SaveFile(const char* filter);
+		static std::string OpenFile(const std::vector<std::string>& filters, const std::string& title = "Open File", const std::string& initialPath = ".", bool multiselect = false);
+		static std::string SaveFile(const std::vector<std::string>& filters, const std::string& title = "Save File", const std::string& initialPath = ".");
 	};
 }
