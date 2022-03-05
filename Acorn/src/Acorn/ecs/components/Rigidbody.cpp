7832eb52c390a4bfd99d3f21cbe1b9080d211ed9
Michael Finger <michael.finger@icytv.de>
2022-03-05 15:17:58 +0100

    Acorn: Added pch to RigidBody Component

--- Acorn/src/Acorn/ecs/components/Rigidbody.cpp
+++ Acorn/src/Acorn/ecs/components/Rigidbody.cpp
@@ -1,3 +1,14 @@
+
+/*
+ * Copyright (c) 2022 Michael Finger
+ *
+ * SPDX-License-Identifier: Apache-2.0 with Commons Clause
+ *
+ * For more Information on the license, see the LICENSE.md file
+ */
+
+#include "acpch.h"
+
 #include "ecs/components/Components.h"
 
 #include <box2d/b2_body.h>
