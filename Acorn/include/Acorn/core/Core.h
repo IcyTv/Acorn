2e3c8a84e944775323fb1dd3e57c6c50b6a659e6
Michael Finger <michael.finger@icytv.de>
2022-03-05 15:15:47 +0100

    fixup! Acorn: Internal MD5 Implementation

--- Acorn/include/Acorn/core/Core.h
+++ Acorn/include/Acorn/core/Core.h
@@ -1,6 +1,16 @@
+
+/*
+ * Copyright (c) 2022 Michael Finger
+ *
+ * SPDX-License-Identifier: Apache-2.0 with Commons Clause
+ *
+ * For more Information on the license, see the LICENSE.md file
+ */
+
 #pragma once
 
 #include "CoreConfig.h"
+#include "Log.h"
 
 #include <memory>
 #include <string_view>
